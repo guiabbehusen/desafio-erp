ba7501d80ebd59f7f5ed69b4eb43baa92c75e914
gui abbehusen <guisoares1601@gmail.com>
2025-02-06 11:49:58 -0300

    Service/UsuarioService.cs

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+594e19beffe7227ee35a1eeeb5d4ab302185f94a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e28c35ca58d6c9a3029bab597704d72e6d3c560")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
